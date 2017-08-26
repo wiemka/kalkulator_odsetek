558f8eb4ae63510c12e5251b761e1e79af5725fd
Magda Wielgosz <wielgosz.m@vp.pl>
2017-08-26 12:03:40 +0200

    ostateczna korekta kalkulatora

--- js/kalkulator.js
+++ js/kalkulator.js
@@ -2,13 +2,24 @@
 
 function obliczZysk(wplata, iloscLat, okresKapitalizacji, oprocentowanie, podatek) {
 
+    if (podatek == true) {
+        var oprocentowanieOpodatkowane = oprocentowanie * 0.81;
+    } else {
+        var oprocentowanieOpodatkowane = oprocentowanie;
+    }
+    var kapital = wplata * Math.pow(1 + (oprocentowanieOpodatkowane / okresKapitalizacji), iloscLat * okresKapitalizacji);
+
+    return parseFloat(kapital).toFixed(2);
 }
 
 function oblicz() {
     var wplata = parseInt(document.getElementById("wplata").value);
-    var iloscLat = parseInt(document.getElementById("ilosc_Lat").value);
-    var okresKapitalizacji = parseInt(document.getElementById("oprocentowanie").value);
+    var iloscLat = parseInt(document.getElementById("ilosc_lat").value);
+    var okresKapitalizacji = parseInt(document.getElementById("okres_kapitalizacji").value);
     var oprocentowanie = parseInt(document.getElementById("oprocentowanie").value) / 100;
-    var podatek = parseInt(document.getElementById("podatek").checked);
+    var podatek = document.getElementById("podatek").checked;
 
+    var wynik = obliczZysk(wplata, iloscLat, okresKapitalizacji, oprocentowanie, podatek);
+
+    document.getElementById("wynik").innerHTML = wynik;
 }
