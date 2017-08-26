eb1e879163eb5d71d390a904a8a09a20869757b9
Magda Wielgosz <wielgosz.m@vp.pl>
2017-08-26 10:51:47 +0200

    dodanie funkcji oblicz

---
+++ js/kalkulator.js
@@ -0,0 +1,10 @@
+"use strict";
+
+function oblicz() {
+    var wplata = parseInt(document.getElementById("wplata").value);
+    var iloscLat = parseInt(document.getElementById("ilosc_Lat").value);
+    var okresKapitalizacji = parseInt(document.getElementById("oprocentowanie").value);
+    var oprocentowanie = parseInt(document.getElementById("oprocentowanie").value);
+    var podatek = parseInt(document.getElementById("podatek").value);
+
+}
