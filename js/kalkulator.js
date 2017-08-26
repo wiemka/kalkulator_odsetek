5cc7307fa583c503b08bac4de1466fb9e75bb398
Magda Wielgosz <wielgosz.m@vp.pl>
2017-08-26 10:55:00 +0200

    funkcja obliczZysk

--- js/kalkulator.js
+++ js/kalkulator.js
@@ -1,10 +1,14 @@
 "use strict";
 
+function obliczZysk(wplata, iloscLat, okresKapitalizacji, oprocentowanie, podatek) {
+
+}
+
 function oblicz() {
     var wplata = parseInt(document.getElementById("wplata").value);
     var iloscLat = parseInt(document.getElementById("ilosc_Lat").value);
     var okresKapitalizacji = parseInt(document.getElementById("oprocentowanie").value);
-    var oprocentowanie = parseInt(document.getElementById("oprocentowanie").value);
-    var podatek = parseInt(document.getElementById("podatek").value);
+    var oprocentowanie = parseInt(document.getElementById("oprocentowanie").value) / 100;
+    var podatek = parseInt(document.getElementById("podatek").checked);
 
 }
